a3b1a6aa87c8d441756952202eaa350bbdbcb25d
KurtSlyCamilleri <104427355+KurtSlyCamilleri@users.noreply.github.com>
2024-04-10 22:54:39 +0200

    Enemy Spawning Pre-Setup

--- Project-Roguelite-Survivor/Assets/Scripts/RandomSpawnSys.cs
+++ Project-Roguelite-Survivor/Assets/Scripts/RandomSpawnSys.cs
@@ -6,9 +6,10 @@ using TMPro;
 public class RandomSpawnSys : MonoBehaviour
 {
 
-    public GameObject fruit;
-    public GameObject Scrap;
-    public GameObject Enemy;
+    public GameObject enemyOne;
+    public GameObject enemyTwo;
+    public GameObject enemyThree;
+    public Transform Player;
     public float x = 0f;
     public static float running = 0; 
     public int spawnOne = 0;
@@ -17,21 +18,18 @@ public class RandomSpawnSys : MonoBehaviour
     public static int spawnRate1 = 3;
     public static int spawnRate2 = 6;
     public static int spawnRate3 = 8;
+    Vector3 posNorth = new Vector3(0, 0, 0);
 
     void Start()
     {
         StartCoroutine("SpawnRoutine1");
-    }
-
-    public void SpawnScrap()
-    {
         StartCoroutine("SpawnRoutine2");
+        StartCoroutine("SpawnRoutine3");
     }
 
-    public void SpawnEnemies()
+    void Update()
     {
-        
-        StartCoroutine("SpawnRoutine3");
+        posNorth = Player.transform.position;
     }
 
     IEnumerator SpawnRoutine1()
@@ -41,72 +39,71 @@ public class RandomSpawnSys : MonoBehaviour
             spawnOne = Random.Range(0,10);
             if(spawnOne == 1)
             {
-                
-                Vector3 pos1 = new Vector3(x-7, transform.position.y + 1);
-                Instantiate(fruit, pos1, Quaternion.identity);
+                posNorth = new Vector3(x -7, transform.position.y + 1);
+                Instantiate(enemyOne, posNorth, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate1);
                 
             }
             else if(spawnOne == 2)
             {
-                Vector3 pos2 = new Vector3(x-5, transform.position.y + 1);
-                Instantiate(fruit, pos2, Quaternion.identity);
+                posNorth = new Vector3(x-5, transform.position.y + 1);
+                Instantiate(enemyOne, posNorth, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate1);
                 
             }
             else if(spawnOne == 3)
             {
-                Vector3 pos2 = new Vector3(x-3, transform.position.y + 1);
-                Instantiate(fruit, pos2, Quaternion.identity);
+                posNorth = new Vector3(x-3, transform.position.y + 1);
+                Instantiate(enemyOne, posNorth, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate1);
                 
             }
             else if(spawnOne == 4)
             {
-                Vector3 pos2 = new Vector3(x-1, transform.position.y + 1);
-                Instantiate(fruit, pos2, Quaternion.identity);
+                posNorth = new Vector3(x-1, transform.position.y + 1);
+                Instantiate(enemyOne, posNorth, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate1);
                 
             }
             else if(spawnOne == 5)
             {
-                Vector3 pos2 = new Vector3(x+1, transform.position.y + 1);
-                Instantiate(fruit, pos2, Quaternion.identity);
+                posNorth = new Vector3(x+1, transform.position.y + 1);
+                Instantiate(enemyOne, posNorth, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate1);
                 
             }
             else if(spawnOne == 6)
             {
-                Vector3 pos2 = new Vector3(x+3, transform.position.y + 1);
-                Instantiate(fruit, pos2, Quaternion.identity);
+                posNorth = new Vector3(x+3, transform.position.y + 1);
+                Instantiate(enemyOne, posNorth, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate1);
                 
             }
             else if(spawnOne == 7)
             {
-                Vector3 pos2 = new Vector3(x+5, transform.position.y + 1);
-                Instantiate(fruit, pos2, Quaternion.identity);
+                posNorth = new Vector3(x+5, transform.position.y + 1);
+                Instantiate(enemyOne, posNorth, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate1);
                 
             }
             else if(spawnOne == 8)
             {
-                Vector3 pos2 = new Vector3(x+7, transform.position.y + 1);
-                Instantiate(fruit, pos2, Quaternion.identity);
+                posNorth = new Vector3(x+7, transform.position.y + 1);
+                Instantiate(enemyOne, posNorth, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate1);
                 
             }
             else if(spawnOne == 9)
             {
-                Vector3 pos2 = new Vector3(x-9, transform.position.y + 1);
-                Instantiate(fruit, pos2, Quaternion.identity);
+                posNorth = new Vector3(x-9, transform.position.y + 1);
+                Instantiate(enemyOne, posNorth, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate1);
                 
             }
             else
             {
-                Vector3 pos2 = new Vector3(x+9, transform.position.y + 1);
-                Instantiate(fruit, pos2, Quaternion.identity);
+                posNorth = new Vector3(x+9, transform.position.y + 1);
+                Instantiate(enemyOne, posNorth, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate1);
                 
             }
@@ -121,70 +118,70 @@ public class RandomSpawnSys : MonoBehaviour
             if(spawnTwo == 1)
             {
                 Vector3 pos1 = new Vector3(x-7, transform.position.y + 1);
-                Instantiate(Scrap, pos1, Quaternion.identity);
+                Instantiate(enemyTwo, pos1, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate2);
                 
             }
             else if(spawnTwo == 2)
             {
                 Vector3 pos2 = new Vector3(x-5, transform.position.y + 1);
-                Instantiate(Scrap, pos2, Quaternion.identity);
+                Instantiate(enemyTwo, pos2, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate2);
                 
             }
             else if(spawnTwo == 3)
             {
                 Vector3 pos2 = new Vector3(x-3, transform.position.y + 1);
-                Instantiate(Scrap, pos2, Quaternion.identity);
+                Instantiate(enemyTwo, pos2, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate2);
                 
             }
             else if(spawnTwo == 4)
             {
                 Vector3 pos2 = new Vector3(x-1, transform.position.y + 1);
-                Instantiate(Scrap, pos2, Quaternion.identity);
+                Instantiate(enemyTwo, pos2, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate2);
                 
             }
             else if(spawnTwo == 5)
             {
                 Vector3 pos2 = new Vector3(x+1, transform.position.y + 1);
-                Instantiate(Scrap, pos2, Quaternion.identity);
+                Instantiate(enemyTwo, pos2, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate2);
                 
             }
             else if(spawnTwo == 6)
             {
                 Vector3 pos2 = new Vector3(x+3, transform.position.y + 1);
-                Instantiate(Scrap, pos2, Quaternion.identity);
+                Instantiate(enemyTwo, pos2, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate2);
                 
             }
             else if(spawnTwo == 7)
             {
                 Vector3 pos2 = new Vector3(x+5, transform.position.y + 1);
-                Instantiate(Scrap, pos2, Quaternion.identity);
+                Instantiate(enemyTwo, pos2, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate2);
                 
             }
             else if(spawnTwo == 8)
             {
                 Vector3 pos2 = new Vector3(x+7, transform.position.y + 1);
-                Instantiate(Scrap, pos2, Quaternion.identity);
+                Instantiate(enemyTwo, pos2, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate2);
                 
             }
             else if(spawnTwo == 9)
             {
                 Vector3 pos2 = new Vector3(x-9, transform.position.y + 1);
-                Instantiate(Scrap, pos2, Quaternion.identity);
+                Instantiate(enemyTwo, pos2, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate2);
                 
             }
             else
             {
                 Vector3 pos2 = new Vector3(x+9, transform.position.y + 1);
-                Instantiate(fruit, pos2, Quaternion.identity);
+                Instantiate(enemyOne, pos2, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate2);
                 
             }
@@ -199,70 +196,70 @@ public class RandomSpawnSys : MonoBehaviour
             if(spawnOne == 1)
             {
                 Vector3 pos1 = new Vector3(x-7, transform.position.y + 1);
-                Instantiate(Enemy, pos1, Quaternion.identity);
+                Instantiate(enemyThree, pos1, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate3);
                 
             }
             else if(spawnThree == 2)
             {
                 Vector3 pos2 = new Vector3(x-5, transform.position.y + 1);
-                Instantiate(Enemy, pos2, Quaternion.identity);
+                Instantiate(enemyThree, pos2, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate3);
                 
             }
             else if(spawnThree == 3)
             {
                 Vector3 pos2 = new Vector3(x-3, transform.position.y + 1);
-                Instantiate(Enemy, pos2, Quaternion.identity);
+                Instantiate(enemyThree, pos2, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate3);
                 
             }
             else if(spawnThree == 4)
             {
                 Vector3 pos2 = new Vector3(x-1, transform.position.y + 1);
-                Instantiate(Enemy, pos2, Quaternion.identity);
+                Instantiate(enemyThree, pos2, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate3);
                 
             }
             else if(spawnThree == 5)
             {
                 Vector3 pos2 = new Vector3(x+1, transform.position.y + 1);
-                Instantiate(Enemy, pos2, Quaternion.identity);
+                Instantiate(enemyThree, pos2, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate3);
                 
             }
             else if(spawnThree == 6)
             {
                 Vector3 pos2 = new Vector3(x+3, transform.position.y + 1);
-                Instantiate(Enemy, pos2, Quaternion.identity);
+                Instantiate(enemyThree, pos2, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate3);
                 
             }
             else if(spawnThree == 7)
             {
                 Vector3 pos2 = new Vector3(x+5, transform.position.y + 1);
-                Instantiate(Enemy, pos2, Quaternion.identity);
+                Instantiate(enemyThree, pos2, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate3);
                 
             }
             else if(spawnThree == 8)
             {
                 Vector3 pos2 = new Vector3(x+7, transform.position.y + 1);
-                Instantiate(Enemy, pos2, Quaternion.identity);
+                Instantiate(enemyThree, pos2, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate3);
                 
             }
             else if(spawnThree == 9)
             {
                 Vector3 pos2 = new Vector3(x-9, transform.position.y + 1);
-                Instantiate(Enemy, pos2, Quaternion.identity);
+                Instantiate(enemyThree, pos2, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate3);
                 
             }
             else
             {
                 Vector3 pos2 = new Vector3(x+9, transform.position.y + 1);
-                Instantiate(Enemy, pos2, Quaternion.identity);
+                Instantiate(enemyThree, pos2, Quaternion.identity);
                 yield return new WaitForSeconds(spawnRate3);
                 
             }
