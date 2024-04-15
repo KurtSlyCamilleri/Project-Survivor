28df9aaf3a83a39bc3a361e0af4abab41e7bb97c
KurtSlyCamilleri <kurt.camilleri.d59046@mcast.edu.mt>
2024-04-15 17:47:51 +0200

    Enemy Difficulty Patch

--- Project-Roguelite-Survivor/Assets/Scripts/Game/RandomSpawnSys.cs
+++ Project-Roguelite-Survivor/Assets/Scripts/Game/RandomSpawnSys.cs
@@ -16,16 +16,16 @@ public class RandomSpawnSys : MonoBehaviour
     public Transform Player;
     public float x = 0f;
     public static float running = 0; 
-    public int spawnRate1 = 5;
-    public int spawnRate2 = 8;
-    public int spawnRate3 = 12;
+    public int spawnRate1 = 6;
+    public int spawnRate2 = 9;
+    public int spawnRate3 = 13;
 
     void Update()
     {
         PlayerPower = PlayerLevelSys.PlayerLevel;
 
 
-            if (PlayerPower == Stage & spawnRate1 > 2)
+            if (PlayerPower == Stage && spawnRate1 > 2)
             {
                 spawnRate1 -= 1;
                 spawnRate2 -= 1;
