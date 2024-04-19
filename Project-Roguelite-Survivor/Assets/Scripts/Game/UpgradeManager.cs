5efc461611fba8dac4ccd429958ff6873aa2f97a
KurtSlyCamilleri <104427355+KurtSlyCamilleri@users.noreply.github.com>
2024-04-19 14:32:51 +0200

    upgrade system test 1

--- Project-Roguelite-Survivor/Assets/Scripts/Game/UpgradeManager.cs
+++ Project-Roguelite-Survivor/Assets/Scripts/Game/UpgradeManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class UpgradeManager : MonoBehaviour
 {
+    public GameObject shieldPack;
+    public GameObject Player;
+    public Transform spawnID;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,22 @@ public class UpgradeManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SpawnShield()
+    {
+        Vector3 spawnID = Player.transform.position;
+
+        
+        Quaternion direction = Quaternion.Euler(0, Player.transform.eulerAngles.y, 0);
+
+
+        (Instantiate(shieldPack, spawnID, direction) as GameObject).transform.parent = Player.transform;
+
+        GameObject[] canvasList = GameObject.FindGameObjectsWithTag("UpgradeUI");
+        foreach (GameObject canvas in canvasList)
+        {
+            canvas.SetActive(false);
+        }
     }
 }
